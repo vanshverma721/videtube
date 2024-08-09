59ca7a8f9d705db7b88cffb4d15ba4d7bfebc0fc
vanshverma721 <vvv.721781@gmail.com>
2024-08-09 17:50:14 +0530

    making header

---
+++ src/components/Header.jsx
@@ -0,0 +1,73 @@
+import React, { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
+import ytLogo from "../images/yt-logo.png";
+import ytLogoMobile from "../images/yt-logo-mobile.png";
+
+import { SlMenu } from "react-icons/sl";
+import { IoIosSearch } from "react-icons/io";
+import { RiVideoAddLine } from "react-icons/ri";
+import { FiBell } from "react-icons/fi";
+import { CgClose } from "react-icons/cg";
+
+import { Context } from "../context/contextApi";
+import Loader from "../shared/loader";
+
+const Header = () => {
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const { loading, mobileMenu, setMobileMenu } = useContext(Context);
+
+  const navigate = useNavigate();
+
+  const searchQueryHandler = (event) => {
+    if (
+      (event?.key === "Enter" || event === "searchButton") &&
+      searchQuery?.length > 0
+    ) {
+      navigate(`/searchResult/${searchQuery}`);
+    }
+  };
+
+  const mobileMenuToggle = () => {
+    setMobileMenu(!mobileMenu);
+  };
+
+  const { pathname } = useLocation();
+  const pageName = pathname?.split("/")?.filter(Boolean)?.[0];
+
+  return (
+    <div className="sticky top-0 z-10 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-white dark:bg-black">
+      {loading && <Loader />}
+
+      <div className="flex h-5 items-center">
+        {pageName !== "video" && (
+          <div
+            className="flex md:hidden md:mr-6 cursor-pointer items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]"
+            onClick={mobileMenuToggle}
+          >
+            {mobileMenu ? (
+              <CgClose className="text-white text-xl" />
+            ) : (
+              <SlMenu className="text-white text-xl" />
+            )}
+          </div>
+        )}
+        <Link to="/" className="flex h-5 items-center">
+          <img
+            className="h-full hidden dark:md:block"
+            src={ytLogo}
+            alt="Youtube"
+          />
+          <img
+            className="h-full md:hidden"
+            src={ytLogoMobile}
+            alt="Youtube"
+          />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Header
